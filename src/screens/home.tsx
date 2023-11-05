6c95d4b99ab8a7cec84b2a9d6a2d102b64b5babf
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-11-05 23:26:16 +0530

    minor fixes

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -8,16 +8,23 @@ import {
   Dimensions,
   FlatList,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {RootScreenStackProps} from '../utils/types';
 import {colors} from '../utils/colors';
 import {ExamYearsData} from '../utils/consts';
 import Accordion from '../components/accordion';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const {height, width} = Dimensions.get('window');
 
 const Home = ({navigation}: RootScreenStackProps<'Home'>) => {
   const [refresh, setRefresh] = React.useState(false);
+  const [examList, setExamList] = React.useState<Array<any>>([]);
+
+  useEffect(() => {
+    setExamList(ExamYearsData);
+  }, []);
+
   return (
     <SafeAreaView
       style={{
@@ -28,31 +35,39 @@ const Home = ({navigation}: RootScreenStackProps<'Home'>) => {
       }}>
       <View style={{width: width * 0.95, marginHorizontal: '2.5%'}}>
         <FlatList
-          data={ExamYearsData}
-          style={{height: height * 0.9}}
-          keyExtractor={(item, index) => item.id.toString()}
+          data={examList}
+          style={{height: height * 0.95}}
+          keyExtractor={(item, index) => index.toString()}
+          stickyHeaderIndices={[0]}
           renderItem={({item, index}) => {
             return (
-              <View>
-                <Accordion
-                  visible={refresh}
-                  title={'Title'}
-                  description={'Description'}
-                  toggleButton={() => {
-                    setRefresh(!refresh);
-                  }}
-                />
-              </View>
+              <Accordion
+                key={index}
+                visible={item.visible}
+                title={item.year + ' #' + (index+1)}
+                toggleButton={() => {
+                  let list = [...examList];
+                  list[index].visible = !list[index].visible;
+                  setExamList([...list]);
+                }}
+                sessionsList={item.examSessions}
+              />
             );
           }}
           ListHeaderComponent={() => {
             return (
-              <View style={{height: height * 0.15, marginTop: '2.5%'}}>
+              <View
+                style={{
+                  height: height * 0.15,
+                  zIndex: 10,
+                  backgroundColor: colors.backgroundPrimary,
+                }}>
                 <Image
                   style={{
                     height: height * 0.1,
                     width: width * 0.8,
                     marginHorizontal: '10%',
+                    marginTop: '2.5%',
                   }}
                   source={require('../assets/logo.png')}
                 />
--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -1,9 +1,106 @@
 export const ExamYearsData = [
   {
     id: 'examId1',
-    year: '2023',
+    year: 'JEE Mains 2023',
     examType: 'JEE',
+    visible: false,
     examSessions: [
+      {
+        title: 'paper1 paper1 paper1 ',
+        id: 'id1',
+      },
+      {
+        title: 'paper2',
+        id: 'id2',
+      },
+      {
+        title: 'paper3',
+        id: 'id3',
+      },
+      {
+        title: 'paper4',
+        id: 'id4',
+      },
+      {
+        title: 'paper1',
+        id: 'id1',
+      },
+      {
+        title: 'paper2',
+        id: 'id2',
+      },
+      {
+        title: 'paper3',
+        id: 'id3',
+      },
+      {
+        title: 'paper4',
+        id: 'id4',
+      },
+    ],
+  },
+  {
+    id: 'examId1',
+    year: 'JEE Mains 2023',
+    examType: 'JEE',
+    visible: false,
+    examSessions: [
+      {
+        title: 'paper1 paper1 paper1 ',
+        id: 'id1',
+      },
+      {
+        title: 'paper2',
+        id: 'id2',
+      },
+      {
+        title: 'paper3',
+        id: 'id3',
+      },
+      {
+        title: 'paper4',
+        id: 'id4',
+      },
+      {
+        title: 'paper1',
+        id: 'id1',
+      },
+      {
+        title: 'paper2',
+        id: 'id2',
+      },
+      {
+        title: 'paper3',
+        id: 'id3',
+      },
+      {
+        title: 'paper4',
+        id: 'id4',
+      },
+    ],
+  },
+  {
+    id: 'examId1',
+    year: 'JEE Mains 2023',
+    examType: 'JEE',
+    visible: false,
+    examSessions: [
+      {
+        title: 'paper1 paper1 paper1 ',
+        id: 'id1',
+      },
+      {
+        title: 'paper2',
+        id: 'id2',
+      },
+      {
+        title: 'paper3',
+        id: 'id3',
+      },
+      {
+        title: 'paper4',
+        id: 'id4',
+      },
       {
         title: 'paper1',
         id: 'id1',
